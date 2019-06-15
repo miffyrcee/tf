07d7ea5f2f15c2824f1e9de83c402d6941cc03d3
miffyrcee <miffyrcee@outlook.com>
2019-06-15 19:16:01 +0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+from io import BytesIO
+
+import matplotlib.pyplot as plt
 import tensorflow as tf
-
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+import tensorflow_estimator
+import tensorflow_hub as hub
+from IPython.display import Image, display
