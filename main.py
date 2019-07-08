5caf1eac974440d246e158d9c134828c550c2066
miffyrcee <miffyrcee@outlook.com>
2019-07-08 18:15:27 +0800

    update

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
-from __future__ import absolute_import, division, print_function
+import os
+import random as r
+import re
+import sys
 
-import glob
-from io import BytesIO
-
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-import tensorflow_estimator
-import tensorflow_hub as hub
-from IPython.display import Image, display
-from tensorflow import keras
+import tensorflow_hub as th
+from tensorflow import io
 
+path = '/home/miffyrcee/Downloads'
+gif_name = [
+    os.path.join(path, i) for i in os.listdir(path)
+    if re.match('(\w)+.jpeg', i)
+]
 (train_images,
  train_labels), (test_images,
                  test_labels) = tf.keras.datasets.mnist.load_data()
 
-train_labels = train_labels[:1000]
-test_labels = test_labels[:1000]
+os.path()
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
 
-train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
-test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
-
-
-def create_model():
-    model = tf.keras.models.Sequential([
-        keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(784, )),
-        keras.layers.Dropout(0.2),
-        keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(),
-                  loss=tf.keras.losses.sparse_categorical_crossentropy,
-                  metrics=['accuracy'])
-
-    return model
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 
-def adjust(image):
-    tf.image.adjust_hue(image)
-    tf.image.adjust_saturation(image)
-    return image
+class mnist_model(tf.keras.Model):
+    def __init__(self):
+        super(mnist_model, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu')
+        self.flatten = tf.keras.layers.Flatten()
+        self.d1 = tf.keras.layers.Dense(128, activation='relu')
+        self.d2 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
 
 
-model = create_model()
-model.fit(train_images, train_labels, epochs=5)
-model.save('my_model.h5')
-
-arr = np.zeros((2, 2), int)
-print(arr)
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = mnist_model(images)
+        loss = tf.losses.SparseCategoricalCrossentropy(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.3)
+if '__name__' == '__main__':
+    pass
