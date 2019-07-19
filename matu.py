292b0f8bbfbd9bd93f906fcfebf78ef122ea1c2f
miffyrcee <miffyrcee@outlook.com>
2019-07-19 19:23:24 +0800

    update

---
+++ matu.py
@@ -0,0 +1,4 @@
+import numpy as np
+import tensorflow as tf
+
+print(np.random.random((4000, 4000)))
--- tf/tf.py
+++ tf/tf.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
-
 """Main module."""
+import scipy
+import tensorflow as tf
+
+c = (scipy.sum(map(lambda x: pow(x, 2), scipy.fft(range(10)))))
+print([i for i in c])
