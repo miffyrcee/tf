d92725790db52b46b3e837433d1e4a6629439368
miffyrcee <miffyrcee@outlook.com>
2019-10-01 10:19:47 +0800

    update

--- SMT_t.py
+++ SMT_t.py
@@ -11,8 +11,13 @@ def att(u, k, v):
     return tf.matmul(att_weights, v)
 
 
+def probabilities(x, y, theta, scaled_factor, t):
+    return x / scaled_factor, y / scaled_factor, tf.sin(theta), tf.cos(
+        theta), tf.math.exp(-t)
+
+
 class att_block(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model):
         super().__init__()
         self.fc = tf.keras.layers.Dense(units)
         self.att_ln = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -30,12 +35,16 @@ class att_block(tf.keras.layers.Layer):
         return self.att_block
 
 
-class encoder(tf.keras.models.Model):
+class encoder_layer(tf.keras.layers.Layer):
     def __init__(self):
         super().__init__()
 
+
+class encoder(tf.keras.models.Model):
+    def __init__(self):
+        super().__init__()
+        self.ln1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.ln2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
     def call(self, mask):
         return att_block(mask, mask)
-print(
-    tf.keras.layers.Dense(10, activation=tf.nn.relu)(tf.random.uniform(
-        (10, 10))))
---
+++ image_test.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+p1 = tf.io.read_file('/home/miffyrcee/Downloads/photo_2019-09-29_18-34-23.jpg')
+print(p1)
